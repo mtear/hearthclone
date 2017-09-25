4a35aaba46f4c2221640bab9832aeef2df6dce8f
Nic Wilson <nic.edward.wilson@gmail.com>
2017-09-25 01:25:33 -0700

    Build 9
    Started the Unity client

---
+++ hearthclone_unity/hearthclone_unity/Assets/ApiBridge.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using UnityEngine;
+
+public class ApiBridge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartClient ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// The port number for the remote device.  
+	private const int port = 1121;
+
+	private static void StartClient()
+	{
+		// Connect to a remote device.  
+		try
+		{
+			// Establish the remote endpoint for the socket.  
+			// The name of the   
+			// remote device is "host.contoso.com".  
+			IPHostEntry ipHostInfo = Dns.Resolve("polybellum.com");
+			IPAddress ipAddress = ipHostInfo.AddressList[0];
+			IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+			// Create a TCP/IP socket.  
+			Socket client = new Socket(AddressFamily.InterNetwork,
+				SocketType.Stream, ProtocolType.Tcp);
+
+			// Connect to the remote endpoint.  
+			client.BeginConnect(remoteEP,
+				new AsyncCallback(ConnectCallback), client);
+			//connectDone.WaitOne();
+
+			//Receive(client);
+
+			// Release the socket.  
+			//Console.WriteLine("Shutting down connection...");
+			//client.Shutdown(SocketShutdown.Both);
+			//client.Close();
+
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+
+	private static void ConnectCallback(IAsyncResult ar)
+	{
+		try
+		{
+			// Retrieve the socket from the state object.  
+			Socket client = (Socket)ar.AsyncState;
+
+			// Complete the connection.  
+			client.EndConnect(ar);
+
+			Debug.Log("Socket connected to " + client.RemoteEndPoint.ToString());
+
+		//	HS_Request r = new HS_Request(name, "NAMESET");
+			//Send(client, r.Json+"<EOF>");
+
+			// Signal that the connection has been made.  
+			//connectDone.Set();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+
+}
---
+++ hearthclone_unity/hearthclone_unity/Assets/CardDragDrop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDragDrop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	bool dragging = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			int mask = LayerMask.GetMask ("PlayerHand");
+			if (Physics.Raycast (ray, out hit, 150.0f, mask)) {
+				dragging = true;
+			}
+		} else if(dragging) {
+			if (Input.GetMouseButtonUp (0)) {
+				dragging = false;
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				int mask = LayerMask.GetMask ("PlayZone");
+				if (Physics.Raycast (ray, out hit, 150.0f, mask)) {
+					Debug.Log ("Playzone");
+					Destroy (gameObject);
+					GameObject zone = GameObject.Find ("creatureportion_0");
+					GameObject prefab = Instantiate (Resources.Load ("oval") as GameObject);
+					prefab.transform.parent = zone.transform;
+				}
+			}
+			else if (Input.GetMouseButton (0)) {
+				Vector3 v = transform.position;
+				Vector3 m = Input.mousePosition;
+				m.z = 100;
+				m = Camera.main.ScreenToWorldPoint (m);
+				//v.x = m.x; v.y = m.y;
+				transform.position = m;
+			}
+		}
+	}
+}
